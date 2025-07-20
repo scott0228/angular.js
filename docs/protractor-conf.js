ed59ec696f6746f477e3a2a6b7387d80e40f205b
scott0228 <scott0228@gmail.com>
2025-07-20 14:27:55 +0800

    feat(config): 修正 chromedriver

--- docs/protractor-conf.js
+++ docs/protractor-conf.js
@@ -6,6 +6,22 @@ config.specs = [
   'app/e2e/**/*.scenario.js'
 ];
 
-config.capabilities.browserName = 'chrome';
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: [
+      '--no-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--headless',
+      '--disable-web-security',
+      '--disable-features=VizDisplayCompositor',
+      '--window-size=1920,1080'
+    ]
+  }
+};
+
+// 指定 ChromeDriver 路徑
+config.chromeDriver = '../node_modules/webdriver-manager/selenium/chromedriver_138.0.7204.157';
 
 exports.config = config;
